dcb9b2e50a60a944acb3b84eee208ff55c7361d2
erikaosgue <1476@holbertonschool.com>
2021-01-18 00:50:09 -0500

    Update start server file

--- start_servers.sh
+++ start_servers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-(cd backend; ./database_files/start_database.sh)
+# ./backend/database_files/stop_database.sh
+# (cd backend; ./database_files/start_database.sh)
 (cd backend; tmux new -s backend -d 'go run server.go') # 8081
 (cd snake_frontend; tmux new -s frontend -d 'npm run serve -- --port 8082')
